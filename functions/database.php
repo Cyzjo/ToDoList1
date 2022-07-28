b7a40a0b9e1d65a6b2b06f1f1abfc206dcb768fb
Cyzjo <57315128+Cyzjo@users.noreply.github.com>
2022-07-28 14:30:24 +0200

    MAJ Acces SQL

---
+++ functions/database.php
@@ -0,0 +1,5 @@
+<?php
+    $servername = '192.168.1.30:3306';
+    $username = 'johann';
+    $password = '311277';
+?>
--- functions/functions.php
+++ functions/functions.php
@@ -1,14 +1,8 @@
 <?php
 
-
-/*Fonction identification */
-
-
 /*Fonction Lecture des tâches*/
 function query($user) {
-    $servername = '192.168.1.30:3306';
-    $username = 'johann';
-    $password = '311277';
+    include ('database.php');
     $conn = new PDO("mysql:host=$servername;dbname=db_zych", $username, $password);
     $sqlQuery = "SELECT * FROM T_todo WHERE visible = 1 AND user = '$user';";
     $query = $conn->query($sqlQuery);
@@ -18,9 +12,7 @@ function query($user) {
 
 /*Fonction ajout tâche */
 function ajout_tache($ajout) {
-    $servername = '192.168.1.30:3306';
-    $username = 'johann';
-    $password = '311277';
+    include ('database.php');
     $conn = new PDO("mysql:host=$servername;dbname=db_zych", $username, $password);
     $sqlQuery = "INSERT INTO T_todo (user, date, tache, statut, visible) VALUES (?, ?, ?, ?, ?)";
     $ajouter_tache = $conn->prepare($sqlQuery);
@@ -29,9 +21,7 @@ function ajout_tache($ajout) {
 
 /*Fonction effacer tâche */
 function delete_tache($id){
-    $servername = '192.168.1.30:3306';
-    $username = 'johann';
-    $password = '311277';
+    include ('database.php');
     $conn = new PDO("mysql:host=$servername;dbname=db_zych", $username, $password);
     $sqlQuery = "UPDATE T_todo SET visible = '0' WHERE id = $id;";
     $conn->query($sqlQuery);
@@ -39,9 +29,7 @@ function delete_tache($id){
 
 /*Fonction statut tâche en terminé */
 function terminer_tache($id){
-    $servername = '192.168.1.30:3306';
-    $username = 'johann';
-    $password = '311277';
+    include ('database.php');
     $conn = new PDO("mysql:host=$servername;dbname=db_zych", $username, $password);
     $sqlQuery = "UPDATE T_todo SET statut = 'Terminé' WHERE id = $id;";
     $conn->query($sqlQuery);
